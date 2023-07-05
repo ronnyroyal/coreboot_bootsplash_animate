fd6b4a57c9acc02fc5fa4417cd46a605a5a8c5d9
ronnyroyal <simon.kaiser111@gmx.de>
2023-07-05 21:02:06 +0200

    fix the terrible image helper script

--- addimages.sh
+++ addimages.sh
@@ -1,91 +1,12 @@
-cp build/coreboot.rom build/coreboot_copy.rom
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash00.jpg -n bootsplash00.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash01.jpg -n bootsplash01.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash02.jpg -n bootsplash02.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash03.jpg -n bootsplash03.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash04.jpg -n bootsplash04.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash05.jpg -n bootsplash05.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash06.jpg -n bootsplash06.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash07.jpg -n bootsplash07.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash08.jpg -n bootsplash08.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash09.jpg -n bootsplash09.jpg -t bootsplash
+#!/bin/bash
+cp -n build/coreboot.rom build/coreboot_copy.rom #backup unmodified rom image
 
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash10.jpg -n bootsplash10.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash11.jpg -n bootsplash11.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash12.jpg -n bootsplash12.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash13.jpg -n bootsplash13.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash14.jpg -n bootsplash14.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash15.jpg -n bootsplash15.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash16.jpg -n bootsplash16.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash17.jpg -n bootsplash17.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash18.jpg -n bootsplash18.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash19.jpg -n bootsplash19.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash20.jpg -n bootsplash20.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash21.jpg -n bootsplash21.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash22.jpg -n bootsplash22.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash23.jpg -n bootsplash23.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash24.jpg -n bootsplash24.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash25.jpg -n bootsplash25.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash26.jpg -n bootsplash26.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash27.jpg -n bootsplash27.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash28.jpg -n bootsplash28.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash29.jpg -n bootsplash29.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash30.jpg -n bootsplash30.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash31.jpg -n bootsplash31.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash32.jpg -n bootsplash32.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash33.jpg -n bootsplash33.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash34.jpg -n bootsplash34.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash35.jpg -n bootsplash35.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash36.jpg -n bootsplash36.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash37.jpg -n bootsplash37.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash38.jpg -n bootsplash38.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash39.jpg -n bootsplash39.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash40.jpg -n bootsplash40.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash41.jpg -n bootsplash41.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash42.jpg -n bootsplash42.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash43.jpg -n bootsplash43.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash44.jpg -n bootsplash44.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash45.jpg -n bootsplash45.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash46.jpg -n bootsplash46.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash47.jpg -n bootsplash47.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash48.jpg -n bootsplash48.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash49.jpg -n bootsplash49.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash50.jpg -n bootsplash50.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash51.jpg -n bootsplash51.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash52.jpg -n bootsplash52.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash53.jpg -n bootsplash53.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash54.jpg -n bootsplash54.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash55.jpg -n bootsplash55.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash56.jpg -n bootsplash56.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash57.jpg -n bootsplash57.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash58.jpg -n bootsplash58.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash59.jpg -n bootsplash59.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash60.jpg -n bootsplash60.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash61.jpg -n bootsplash61.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash62.jpg -n bootsplash62.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash63.jpg -n bootsplash63.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash64.jpg -n bootsplash64.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash65.jpg -n bootsplash65.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash66.jpg -n bootsplash66.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash67.jpg -n bootsplash67.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash68.jpg -n bootsplash68.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash69.jpg -n bootsplash69.jpg -t bootsplash
-
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash70.jpg -n bootsplash70.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash71.jpg -n bootsplash71.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash72.jpg -n bootsplash72.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash73.jpg -n bootsplash73.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash74.jpg -n bootsplash74.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash75.jpg -n bootsplash75.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash76.jpg -n bootsplash76.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash77.jpg -n bootsplash77.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash78.jpg -n bootsplash78.jpg -t bootsplash
-./build/cbfstool build/coreboot.rom add -f example_images/bootsplash79.jpg -n bootsplash79.jpg -t bootsplash
+for filename in example_images/*; 
+do 
+    #echo " ${filename}"; 
+    #echo ${filename#*/}
+    ./build/cbfstool build/coreboot.rom add -f ${filename} -n ${filename#*/} -t bootsplash
+done
 
 ./build/cbfstool build/coreboot.rom add-int -i 1500 -n etc/boot-menu-wait
 ./build/cbfstool build/coreboot.rom add-int -i 80 -n etc/n-of-img
