bd23eb32b177c4b3a036bc0e89aa4fb480a48d54
ronnyroyal <simon.kaiser111@gmx.de>
2023-07-05 22:21:24 +0200

    further improvements to the image helper script

--- addimages.sh
+++ addimages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cp -n build/coreboot.rom build/coreboot_copy.rom #backup unmodified rom image
 
-for filename in example_images/*; 
+for filename in example_images/*; #add all files in /example_images/ to the CBFS
 do 
-    #echo " ${filename}"; 
-    #echo ${filename#*/}
     ./build/cbfstool build/coreboot.rom add -f ${filename} -n ${filename#*/} -t bootsplash
 done
 
-./build/cbfstool build/coreboot.rom add-int -i 1500 -n etc/boot-menu-wait
-./build/cbfstool build/coreboot.rom add-int -i 80 -n etc/n-of-img
+./build/cbfstool build/coreboot.rom add-int -i 1500 -n etc/boot-menu-wait #define boot menu timeout
+./build/cbfstool build/coreboot.rom add-int -i $(ls example_images/ | wc -l) -n etc/n-of-img #set image number variable in CBFS
+
+./build/cbfstool build/coreboot.rom #print CBFS content
